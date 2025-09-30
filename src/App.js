e7266d7916618fe8060fa443b148d8e12f9977db
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-09-30 18:16:16 +0530

    feat: installed and configured tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <h1>Youtube Clone</h1>
+      <h1 className="bg-black text-white text-3xl font-bold">Youtube Clone</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html", // Adjust if your components are in a different location
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
